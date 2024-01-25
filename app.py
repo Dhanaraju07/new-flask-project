f84ee29339669365937b0218d38c434d7e00fff1
Dhanaraju@07 <naramsettydhanaraj@gmail.com>
2024-01-26 00:33:39 +0530

    updated

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, request
 import pymysql
 
+
 app = Flask(__name__)
 
 conn = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="Dhanaraju@07",
-    db="users"
-)
+        host= "localhost",
+        port = 3306,
+        user = "root",
+        password = "Dhanaraju@07",
+        db = "users"
+        
+        )
 
-def insert_details(name, age, city):
-    cur = conn.cursor()
-    cur.execute("INSERT INTO user (name, age, city) VALUES (%s, %s, %s)", (name, age, city))
+
+def insert_details(name,age,city):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO user (name,age, city) VALUES (%s,%s,%s)", (name,age,city))
     conn.commit()
 
 def get_details():
-    cur = conn.cursor()
+    cur=conn.cursor()
     cur.execute("SELECT *  FROM user")
     details = cur.fetchall()
     return details
@@ -28,7 +31,7 @@ def home():
 
 @app.route('/users')
 def get_users_data():
-    details = get_details()
+    details = db.get_details()
     return render_template('users.html', users=details)
 
 @app.route('/create_user', methods=['GET', 'POST'])
@@ -37,11 +40,12 @@ def create():
         name = request.form['name']
         age = request.form['age']
         city = request.form['city']
-        insert_details(name, age, city)
-        details = get_details()
-        return render_template('create_user.html', var=details[-1])
+        db.insert_details(name, age, city)
+        details = db.get_details()
+        return render_template('create_user.html', var=details[-1]) 
     else:
         return render_template('create_user.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
