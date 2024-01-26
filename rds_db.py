62dcc307c5d7095e99a76cb120ca18a694d3e30f
Dhanaraju@07 <naramsettydhanaraj@gmail.com>
2024-01-26 18:35:58 +0530

    updated

--- rds_db.py
+++ rds_db.py
@@ -9,11 +9,13 @@ conn = pymysql.connect(
 
 def insert_details(name, age, city):
     cur = conn.cursor()
+    cur.execute('CREATE DATABASE IF NOT EXISTS users')
     cur.execute("INSERT INTO user (username, age, city) VALUES (%s, %s, %s)", (name, age, city))
     conn.commit()
 
 def get_details():
     cur = conn.cursor()
+    cur.execute('CREATE DATABASE IF NOT EXISTS users')
     cur.execute("SELECT * FROM user")
     details = cur.fetchall()
     return details
