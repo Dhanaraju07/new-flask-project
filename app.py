a1538ec42c76cc38170b4c5889f94843e2ecaaa5
Dhanaraju@07 <naramsettydhanaraj@gmail.com>
2024-01-26 16:13:06 +0530

    updated

--- app.py
+++ app.py
@@ -1,26 +1,8 @@
 from flask import Flask, render_template, request
-import pymysql
 
 app = Flask(__name__)
 
-conn = pymysql.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    password="Dhanaraju@07",
-    db="users"
-)
-
-def insert_details(name, age, city):
-    cur = conn.cursor()
-    cur.execute("INSERT INTO user (userName, userAge, userCity) VALUES (%s, %s, %s)", (name, age, city))
-    conn.commit()
-
-def get_details():
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM user")
-    details = cur.fetchall()
-    return details
+import rds_db as db
 
 @app.route('/')
 def home():
@@ -28,17 +10,17 @@ def home():
 
 @app.route('/users')
 def get_users_data():
-    details = get_details()
+    details = db.get_details()
     return render_template('users.html', users=details)
 
 @app.route('/create_user', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        name = request.form['userName']
-        age = request.form['userAge']
-        city = request.form['userCity']
-        insert_details(name, age, city)
-        details = get_details()
+        name = request.form['username']
+        age = request.form['age']
+        city = request.form['city']
+        db.insert_details(name, age, city)
+        details = db.get_details()
         return render_template('create_user.html', var=details[-1])
     else:
         return render_template('create_user.html')
---
+++ rds_db.py
@@ -0,0 +1,20 @@
+import pymysql
+
+conn = pymysql.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="Dhanaraju@07",
+    db="users"
+)
+
+def insert_details(name, age, city):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO user (username, age, city) VALUES (%s, %s, %s)", (name, age, city))
+    conn.commit()
+
+def get_details():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM user")
+    details = cur.fetchall()
+    return details
